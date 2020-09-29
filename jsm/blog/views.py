3a4ec32387f7b09929e7b444dc73e0432eedbfd3
piyx <piyx.abhishek@gmail.com>
2020-09-29 17:28:44 +0530

    added templates to blog

--- jsm/blog/views.py
+++ jsm/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        'author': 'Nyx',
+        'title': 'Blog post 1',
+        'content': 'First post',
+        'date': 'Aug 26 2020'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog post 2',
+        'content': 'Second post',
+        'date': 'Aug 27 2020'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
