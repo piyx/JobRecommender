8bd4c9cb7cddc1da217b2fb263740cb27de8f3fd
piyx <piyx.abhishek@gmail.com>
2020-09-30 10:48:17 +0530

    added entries to db

--- jsm/blog/views.py
+++ jsm/blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        'author': 'Nyx',
-        'title': 'Blog post 1',
-        'content': 'First post',
-        'date': 'Aug 26 2020'
-    },
-    {
-        'author': 'Jane',
-        'title': 'Blog post 2',
-        'content': 'Second post',
-        'date': 'Aug 27 2020'
-    },
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
